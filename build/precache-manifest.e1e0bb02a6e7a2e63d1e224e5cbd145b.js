fa1a141411968476a605f34d19b26d7aa3892661
wmm <wangmm1@ifeng.com>
2020-02-09 20:11:59 +0800

    ncp build

---
+++ build/precache-manifest.e1e0bb02a6e7a2e63d1e224e5cbd145b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bca5b7322add0a75f5234c8326d99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8422c811d45615d1634",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5b8fedb7d76e94cd839",
+    "url": "/static/js/2.76b6097b.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "/static/js/2.76b6097b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8422c811d45615d1634",
+    "url": "/static/js/main.840307b9.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
